77236cafbb8e846ac5b9456e6f0f689cf2799714
g-alvaro-ti <g.alvaro.ti@gmail.com>
2026-05-18 22:34:58 -0300

    Conteúdo colapsável e Reorganização da aba Jogo

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
+// Para preenchimento dos tabuleiros com divs
 let tabuleiroJogador = document.querySelector(".tabuleiro-jogador");
 let tabuleiroInimigo = document.querySelector(".tabuleiro-inimigo");
+
+// Para os botões de conteúdo colapsáveis
+const botoes = document.querySelectorAll(".colapsavel-expandir");
+const conteudo = document.querySelectorAll(".colapsavel-conteudo");
+
+// Para a ocultação/exibição dos tabuleiros
+const botaoIniciar = document.querySelector(".botao-iniciar-jogo");
+let transparenciaJogador = false;
+let transparenciaInimigo = false;
+
+
+// Função para preencher os tabuleiros da batalha naval com uma grade de 10x10 divs
 
 function criarTabuleiro(tabuleiro) {
   tabuleiro.innerHTML = "";
@@ -28,3 +41,55 @@ function criarTabuleiro(tabuleiro) {
 
 criarTabuleiro(tabuleiroJogador);
 criarTabuleiro(tabuleiroInimigo);
+
+// Função para ocultar/exibir conteúdos colapsáveis
+
+botoes.forEach(botao => {
+    botao.addEventListener("click", () => {
+        const conteudo = botao.nextElementSibling;
+
+        const displayAtual = getComputedStyle(conteudo).display;
+
+        if (displayAtual === "none") {
+            conteudo.style.display = "block";
+        } else {
+            conteudo.style.display = "none";
+        }
+    });
+});
+
+conteudo.forEach(cont => {
+    cont.style.display = "none";
+})
+
+// Função para ativar/desativar transparência de um tabuleiro
+
+function alternarTransparenciaTabuleiro(tipo) {
+
+    if (tipo === "jogador") {
+        transparenciaJogador = !transparenciaJogador;
+
+        tabuleiroJogador.classList.toggle(
+            "tabuleiro-transparente",
+            transparenciaJogador
+        );
+    }
+
+    if (tipo === "inimigo") {
+        transparenciaInimigo = !transparenciaInimigo;
+
+        tabuleiroInimigo.classList.toggle(
+            "tabuleiro-transparente",
+            transparenciaInimigo
+        );
+    }
+}
+
+// Executa uma vez ao abrir o site
+alternarTransparenciaTabuleiro("jogador");
+//alternarTransparenciaTabuleiro("inimigo");
+
+botaoIniciar.addEventListener("click", () =>{
+    alternarTransparenciaTabuleiro("jogador");
+    alternarTransparenciaTabuleiro("inimigo");
+});
